eecf71b7c8224641140b26496231df69e550e51f
Ruslan <rusel91@idz.ru>
2019-05-10 19:41:53 +0400

    fix

--- app/Http/Controllers/ApiV1/Auth/AuthController.php
+++ app/Http/Controllers/ApiV1/Auth/AuthController.php
@@ -72,7 +72,12 @@ class AuthController extends Controller
         $credentials = $request->only('email', 'password'); // grab credentials from the request
         try {
             if (!$token = JWTAuth::attempt($credentials)) { // attempt to verify the credentials and create a token for the user
-                return response()->json(['error' => 'invalid_credentials'], 401);
+                return response()->json([
+                    'success' => false,
+                    'errors' => [
+                        'email' => 'Неправильный логин или пароль'
+                    ]
+                ], 422);
             }
         } catch (JWTException $e) {
             return response()->json(['error' => 'could_not_create_token'], 500); // something went wrong whilst attempting to encode the token
@@ -113,7 +118,7 @@ class AuthController extends Controller
     {
         auth()->logout();
 
-        return response()->json(['message' => 'Successfully logged out']);
+        return response()->json(['success' => 'true']);
     }
 
     /**
