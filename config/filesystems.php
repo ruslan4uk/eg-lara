f382fdf711713c34cde04a581b790db8a7e7d923
Ruslan <rusel91@idz.ru>
2019-05-24 01:57:40 +0400

    fix

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DRIVER', 's3'),
 
     /*
     |--------------------------------------------------------------------------
@@ -57,10 +57,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'key' => env('AWS_ACCESS_KEY_ID', 'AKIAQXEWTB2GWIJYV6GK'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY', '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'),
             'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
+            'bucket' => env('AWS_BUCKET', 'excursguide'),
             'url' => env('AWS_URL'),
         ],
 
