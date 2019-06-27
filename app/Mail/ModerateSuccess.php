2e44f619257947419c26cca636070ff601764f09
Ruslan <rusel91@idz.ru>
2019-06-27 13:18:51 +0400

    fix mail

--- app/Mail/ModerateSuccess.php
+++ app/Mail/ModerateSuccess.php
@@ -18,7 +18,7 @@ class ModerateSuccess extends Mailable
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($user)
     {
         $this->user = $user;
     }
